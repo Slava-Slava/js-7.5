acdc42d9d07e4a141fa329fdb5107ed141b7d60d
Slava Vasiliev <sb-fantom@yandex.ru>
2023-10-07 22:02:15 +0300

    2README.md

--- net.test.js
+++ net.test.js
@@ -15,7 +15,7 @@ afterEach(() => {
 
 describe("Ticket booking test", () => {
   test("Book an affordable ticket'", async () => {
-    await clickElement(page, "body > nav > a:nth-child(2)");
+    await clickElement(page, "body > nav > a:nth-child(4)");
     await clickElement(page, "body > main > section:nth-child(1) > div:nth-child(2) > ul > li > a");
     await clickElement(page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(1) > span:nth-child(5)");
     await clickElement(page, "body > main > section > button");
@@ -23,9 +23,9 @@ describe("Ticket booking test", () => {
     const actual = await getText(page, "body > main > section > div > button");
     expect(actual).contain("Получить код бронирования");
   });
-  
+
   test("Book three free tickets", async () => {
-    await clickElement(page, "body > nav > a:nth-child(2)");
+    await clickElement(page, "body > nav > a:nth-child(4)");
     await clickElement(page, "body > main > section:nth-child(1) > div:nth-child(2) > ul > li > a");
     await clickElement(page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(1) > span:nth-child(3)");
     await clickElement(page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(1) > span:nth-child(2)");
@@ -38,7 +38,7 @@ describe("Ticket booking test", () => {
   });
   
   test("Book a ticket for the purchased seat", async () => {
-    await clickElement(page, "body > nav > a:nth-child(2)");
+    await clickElement(page, "body > nav > a:nth-child(4)");
     await clickElement(page, "body > main > section:nth-child(1) > div:nth-child(2) > ul > li > a");
     await clickElement(page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(1) > span:nth-child(1)");
     expect(String(await page.$eval("button", (button) => { return button.disabled;
